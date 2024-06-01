42dd5527dc2816f59c516188a75cd03eb741e567
Aadarsha Khatri <130676530+AadarshaKhatri@users.noreply.github.com>
2024-06-01 18:24:02 +0545

    Rework

--- src/main/java/org/example/Nepal.java
+++ src/main/java/org/example/Nepal.java
@@ -2,16 +2,22 @@ package org.example;
 
 public class Nepal {
     public static void main(String[] args) {
-
-
-        for (int i = 1; i <= 5; i++) {
-            for(int j = 1 ; j<= i ;j++){
-                System.out.print("*");
-
+        for(int k =0 ; k<2;k++) {
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j <= i; j++) {
+                    System.out.print(" * ");
+                }
+                System.out.println();
             }
+        }
 
-            System.out.println();
+
+        for(int w = 0 ; w < 4 ; w++){
+            System.out.println("*");
+
         }
 
     }
+
 }
+
